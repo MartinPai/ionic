ebae135591e8c392fd1c9ac6d6ef06cb3a4b3500
Martin Ortega <martinortegavasqz@gmail.com>
2023-02-13 12:04:13 -0500

    primercomentario

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import { AlertController } from '@ionic/angular';
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['style.css']
+})
+export class Tab3Page {
+
+  constructor(private alertController: AlertController) {}
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: ['OK','hola','hola'],
+    });
+
+    await alert.present();
+  }
+  
+}
+
+
